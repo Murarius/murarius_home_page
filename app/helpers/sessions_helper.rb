3309bb0dcaa317179a1289b89ea46e95b02bd533
Dariusz Murawski <dariusz.murawski@op.pl>
2016-01-27 13:04:58 +0100

    show appriopriary icons when user looged in/out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
+# module SessionsHelper
 module SessionsHelper
+
+  def current_user
+    @current_user ||= Owner.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
