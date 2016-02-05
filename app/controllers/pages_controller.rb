86c2c871dfdc1315cdf4d8a4f243ec7a78f5f243
Murarius <murarius2011@gmail.com>
2016-02-05 11:47:55 +0100

    tweek start#contact controller,  add contact_message#contact controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 # class PagesController < ApplicationController
 class PagesController < ApplicationController
   def start
+    @message = Message.new
+  end
+
+  def contact_message
+    @message = Message.new(params[:message][:from], params[:message][:subject], params[:message][:content])
+    SimpleMailer.send_message(@message).deliver_later
+    render 'start'
   end
 end
