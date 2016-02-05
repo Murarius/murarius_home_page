395cceeb0ea4b23c72e99abb0b1917c4f1a1a754
Murarius <murarius2011@gmail.com>
2016-02-05 11:44:31 +0100

    add message model with route to send_message

---
+++ app/models/message.rb
@@ -0,0 +1,35 @@
+# class Message
+class Message
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+  include GlobalID::Identification
+
+  attr_accessor :from
+  attr_accessor :subject
+  attr_accessor :content
+
+  @all = []
+  def self.all
+    @all
+  end
+
+  def initialize(from = nil, subject = nil, content = nil)
+    @from = from
+    @subject = subject
+    @content = content
+    self.class.all << self
+  end
+
+  def persisted?
+    false
+  end
+
+  def self.find(id)
+    all.detect { |item| item.id.to_s == id.to_s }
+  end
+
+  def id
+    object_id
+  end
+end
