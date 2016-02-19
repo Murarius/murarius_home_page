5b7916ddc3b40162ee7f53cb478ccffa14732bde
Murarius <murarius2011@gmail.com>
2016-02-19 12:20:18 +0100

    add loading icon on work partial

---
+++ app/assets/javascripts/work.js
@@ -0,0 +1,14 @@
+jQuery(function() {
+  $('.project-thumb').each(function() {
+    var base = this;
+    $(base).hide();
+    var tmpImg = new Image();
+    var bg = $(this).css('background-image');
+    bg = bg.replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
+    tmpImg.src = bg;
+    tmpImg.onload = function() {
+      $(base).show();
+      $(base).next().hide();
+    };
+  });
+});
