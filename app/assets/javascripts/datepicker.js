96b532e7e0ebe685402bc73af3fb81605653a9ee
Murarius <murarius2011@gmail.com>
2016-02-25 11:29:42 +0100

    add links now open in new tab, changed date format in datepicker

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,7 +2,7 @@ jQuery(function() {
   $( "#datepicker" ).datepicker({
     changeMonth: true,
     changeYear: true,
-    // dateFormat: 'MM yy',
+    dateFormat: 'yy-mm-dd',
     minDate: "-4Y",
     maxDate: "+1Y"
   });
